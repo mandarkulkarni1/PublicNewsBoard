28dfbff7284a28e58a155121a2d19ce1690078cf
Mandar Uday Kulkarni <mandarkul@cybage.com>
2021-06-28 04:05:27 +0530

    Final Build v1

--- Back-end-server/databaseCredential.js
+++ Back-end-server/databaseCredential.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'admin123',
-  database: 'testdb',
+  password: '1234',
+  database: 'finaldb',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
